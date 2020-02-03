3bb8c3cafdff53f41c1941acb6e4006359386801
Connor Clifford <co316665@ucf.edu>
2020-02-03 10:20:34 -0500

    2.3.20 ICE

--- ice/w5a/src/index.js
+++ ice/w5a/src/index.js
@@ -1,16 +1,35 @@
+let element = undefined;
+let taskInput = undefined;
+
 function runOnLoad()
 {
     // Create a container for us
-    let element = document.createElement("div");
+    element = document.createElement("div");
     element.id = "container";
     document.body.appendChild(element);
 
     // Handle adding a new task
     var addTaskButton = document.getElementById("addTask");
     addTaskButton.addEventListener("click", onClick)
+    
 }
+
 function onClick() {
     console.log("clicked!");
+
+    let newTask = document.createElement("div");
+    let input = document.createElement("input");
+    input.type ="checkbox";
+    let span = document.createElement("span");
+    span.innerHTML = taskInput.value;
+    newTask.appendChild(input);
+    newTask.appendChild(span);
+    
+    function onChange(){
+        console.log(span.innerHTML);
+    }
+    input.addEventListener("change", onChange);
 }
+
 
 window.addEventListener("DOMContentLoaded", runOnLoad);
